74fecedd6b9aba901d46bc717b467c9aaea250dd
kburhan <kburhan07@gmail.com>
2020-04-05 15:27:21 -0400

    cmt

--- src/test/java/com/weborders/pages/AbstractBasePage.java
+++ src/test/java/com/weborders/pages/AbstractBasePage.java
@@ -5,9 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-/**
- * It meant to be extended
- */
+
 public abstract class AbstractBasePage {
     protected WebDriver driver = Driver.getDriver();
     protected WebDriverWait wait = new WebDriverWait(driver, 20);
--- src/test/java/com/weborders/tests/AbstractBaseTest.java
+++ src/test/java/com/weborders/tests/AbstractBaseTest.java
@@ -1,4 +1,46 @@
 package com.weborders.tests;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.weborders.utilities.BrowserUtilities;
+import com.weborders.utilities.ConfigurationReader;
+import com.weborders.utilities.Driver;
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+
 public abstract class AbstractBaseTest {
+    protected WebDriver driver = Driver.getDriver();
+
+    protected static ExtentReports extentReports;
+    protected static ExtentHtmlReporter extentHtmlReporter;
+    protected static ExtentTest extentTest;
+
+    @BeforeTest
+    public void beforeTest(){
+
+    }
+    @AfterTest
+    public void afterTest(){
+
+    }
+
+    @BeforeMethod
+    public void setup(){
+        driver.get(ConfigurationReader.getProperty("ur"));
+        driver.manage().window().maximize();
+    }
+    @AfterMethod
+    public void teardown(ITestResult testResult){
+        if(testResult.getStatus()==ITestResult.FAILURE) {
+            String screenshotLocation = BrowserUtilities.getScreenshot(testResult.getName());
+
+        }
+        BrowserUtilities.wait(3);
+        Driver.closeDriver();
+    }
 }
